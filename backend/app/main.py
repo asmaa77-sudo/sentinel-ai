0906c6997e630fec42230ed0ea4a7a9b4d7d1c23
Asmaa Abusamra <asmaaabusamra@Asmaas-MacBook-Air.local>
2026-07-15 09:44:38 -0400

    Add database connection and user model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from app.database.connection import engine, Base
+from app import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Sentinel AI",
